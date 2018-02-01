1ad6db2c14f967a4d79d3de9bae639604777c5b9
musto <musto@users.noreply.github.com>
2018-02-01 22:10:41 +0200

    Fixed Array2 indexing bugs.

--- hivemind/include/hive_array2.h
+++ hivemind/include/hive_array2.h
@@ -8,53 +8,73 @@ namespace hivemind {
   template<typename T>
   class Array2 {
   private:
-    size_t width_, height_;
+    size_t width_;
+    size_t height_;
     T* data_;
   public:
-    Array2( size_t width = 1, size_t height = 1, T* data = nullptr ):
-    width_( 0 ), height_( 0 ), data_( nullptr )
+
+    explicit Array2( size_t width = 1, size_t height = 1 ):
+      width_( 0 ),
+      height_( 0 ),
+      data_( nullptr )
     {
       resize( width, height );
     }
-    Array2( const Array2<T>& rhs ):
-    width_( 0 ), height_( 0 ), data_( nullptr )
+
+    Array2( const Array2& rhs ):
+      width_( 0 ),
+      height_( 0 ),
+      data_( nullptr )
     {
       resize( rhs.width(), rhs.height() );
       memcpy( data_, rhs.data_, width_ * height_ * sizeof( T ) );
     }
-    const Array2& operator = ( const Array2<T>& rhs )
+
+    Array2& operator = ( const Array2& rhs )
     {
       resize( rhs.width(), rhs.height() );
       memcpy( data_, rhs.data_, width_ * height_ * sizeof( T ) );
       return *this;
     }
+
     ~Array2()
     {
       if ( data_ )
         free( data_ );
     }
+
     const bool empty() const { return ( width_ == 0 && height_ == 0 ); }
     inline T* data() { return data_; }
+    inline const T* data() const { return data_; }
     inline size_t width() const { return width_; }
     inline size_t height() const { return height_; }
+
     T* column( size_t n )
     {
-      size_t index = ( n * width_ );
+      assert(n < width_);
+
+      size_t index = ( n * height_ );
       return &data_[index];
     }
-    T* column( size_t n ) const
+
+    const T* column( size_t n ) const
     {
-      size_t index = ( n * width_ );
+      assert(n < width_);
+
+      size_t index = ( n * height_ );
       return &data_[index];
     }
+
     inline T* operator [] ( size_t n )
     {
       return column( n );
     }
-    inline T* operator [] ( size_t n ) const
+
+    inline const T* operator [] ( size_t n ) const
     {
       return column( n );
     }
+
     void resize( size_t width, size_t height )
     {
       width_ = width;
@@ -65,6 +85,7 @@ namespace hivemind {
       else
         data_ = (T*)realloc( data_, width_ * height_ * sizeof( T ) );
     }
+
     void reset( T value )
     {
       if ( !data_ )
